f72af862a2daffd3adb1af6b4f066017201ce422
Jorge Melis <melis.jorge@gmail.com>
2022-11-13 18:50:38 +0100

    first commit

---
+++ lib/info.py
@@ -0,0 +1,27 @@
+import os
+import requests
+
+
+def get_lines(url):
+    response = requests.get(url)
+    response.encoding = response.apparent_encoding
+    data = response.text
+    lines = data.split("\n")
+    lines = [line.rstrip() for line in lines]
+    return lines
+
+
+urls = {
+    "pj_contains": "https://docs.google.com/spreadsheets/d/e/2PACX-1vRr7ArDmTqBjZiDXksNv76d9KTz_dEOSJSSPQwVGFYgiTZOSK2qEsplJMm-Ggwa5QbYsj-ZaNHn1ymg/pub?gid=1466763224&single=true&output=csv",
+    "pj_ends_with": "https://docs.google.com/spreadsheets/d/e/2PACX-1vRr7ArDmTqBjZiDXksNv76d9KTz_dEOSJSSPQwVGFYgiTZOSK2qEsplJMm-Ggwa5QbYsj-ZaNHn1ymg/pub?gid=824889827&single=true&output=csv",
+    "aer_contains": "https://docs.google.com/spreadsheets/d/e/2PACX-1vRr7ArDmTqBjZiDXksNv76d9KTz_dEOSJSSPQwVGFYgiTZOSK2qEsplJMm-Ggwa5QbYsj-ZaNHn1ymg/pub?gid=0&single=true&output=csv",
+    "list_strings": "https://docs.google.com/spreadsheets/d/e/2PACX-1vRr7ArDmTqBjZiDXksNv76d9KTz_dEOSJSSPQwVGFYgiTZOSK2qEsplJMm-Ggwa5QbYsj-ZaNHn1ymg/pub?gid=1185843739&single=true&output=csv",
+}
+
+
+def get_info():
+    info = {}
+    for key in urls:
+        lines = get_lines(urls[key])
+        info[key] = lines
+    return info
---
+++ persona.py
@@ -0,0 +1,62 @@
+import os, sys, shutil
+import openpyxl
+import pandas as pd
+
+from lib import info as inf
+
+
+def personalidad(beneficiario, info):
+    ret = "PF"
+    pj_contains = info["pj_contains"]
+    pj_ends_with = info["pj_ends_with"]
+    aer_contains = info["aer_contains"]
+
+    if any(x.lower() in beneficiario.lower() for x in aer_contains):
+        ret = "AER"
+    if any(x.lower() in beneficiario.lower() for x in pj_contains):
+        ret = "PJ"
+    if any(beneficiario.lower().endswith(x.lower()) for x in pj_ends_with):
+        ret = "PJ"
+    return ret
+
+
+info = inf.get_info()
+# print(type(info), info)
+# sys.exit()
+
+# Fichero limpio (sin tipo de persona)
+xlsin = "/Users/jorge/git/agric/data/2021_beneficiarios_original.xlsx"
+xlspath = "/Users/jorge/git/agric/data/2021_beneficiarios.xlsx"
+shutil.copy(xlsin, xlspath)
+
+# relleno de personalidad
+book = openpyxl.load_workbook(xlspath)
+sheet = book["Sheet1"]
+sheet.cell(row=1, column=4).value = "Tipo"
+for nrow in range(2, sheet.max_row + 1):
+    beneficiario = sheet.cell(row=nrow, column=1).value
+    pj = personalidad(beneficiario, info)
+    sheet.cell(row=nrow, column=4).value = pj
+    # print(nrow, beneficiario, pj)
+book.save(xlspath)
+
+
+xlspath = "/Users/jorge/git/agric/data/2021_beneficiarios.xlsx"
+dfin = pd.read_excel(xlspath)
+# print(dfin)
+dfpj = dfin[dfin["Tipo"] == "PJ"]
+dfaer = dfin[dfin["Tipo"] == "AER"]
+dfpf = dfin[dfin["Tipo"] == "PF"]
+# string = "herederos"
+# dftmp = dfin[dfin["Beneficiario"].str.contains(string, case=False)]
+
+xlsout = "/Users/jorge/git/agric/data/2021_personas.xlsx"
+with pd.ExcelWriter(xlsout, engine="xlsxwriter") as writer:
+    dfpj.to_excel(writer, sheet_name="PJ", index=False)
+    dfaer.to_excel(writer, sheet_name="AER", index=False)
+    dfpf.to_excel(writer, sheet_name="PF", index=False)
+    for string in info["list_strings"]:
+        dftmp = dfin[dfin["Beneficiario"].str.contains(string, case=False)]
+        dftmp.to_excel(writer, sheet_name=string, index=False)
+
+print("done")
